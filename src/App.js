c4924485a7c4be085c68d4c783b77454f3d41b84
Rikke1992 <vladimirp300@gmail.com>
2021-02-15 21:36:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <header><img src='https://cdn.logo.com/hotlink-ok/logo-social-sq.png' alt="LogoPicture"></img><h1>Logo</h1></header>
+      <div className="wraper">
+        <nav><ul>
+          <li>Profile</li>
+          <li>Message</li>
+          <li>News</li>
+          <li>Music</li>
+          <li>Settings</li>
+        </ul></nav>
+        <main><img src="https://cuesc.org.ua/wp-content/uploads/2018/12/France_1920_800_homepage.jpg" alt="LogoWraper"></img></main>
+      </div>
+    </div >
   );
 }
 
